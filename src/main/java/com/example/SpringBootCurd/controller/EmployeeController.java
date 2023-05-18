4aa6addc6c0685f8f0b0bb38dcdeb3ff4543d0ab
unknown <galimadhu.javaworld@gmail.com>
2023-05-18 15:08:42 +0545

    employee contoller created the with msg method

---
+++ src/main/java/com/example/SpringBootCurd/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.example.SpringBootCurd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emp")
+public class EmployeeController {
+	
+	  @GetMapping("/mesg")
+	  public String getMeaasage() {
+		  return "Hi I am spring boot";
+	  }
+
+}
